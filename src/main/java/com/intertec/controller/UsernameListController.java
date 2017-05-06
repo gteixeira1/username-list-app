e9dc55b1d81606f59c547c3ea268f6433803d891
Guilherme Teixeira <Guilherme_Teixeira@gap.com>
2017-05-06 19:52:13 -0300

    Created basic configuration for H2 embedded database

--- src/main/java/com/intertec/controller/UsernameListController.java
+++ src/main/java/com/intertec/controller/UsernameListController.java
@@ -1,6 +1,8 @@
 package com.intertec.controller;
 
 import com.intertec.service.UsernameListService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UsernameListController {
 
+    private static final Log LOG = LogFactory.getLog(UsernameListController.class);
+
     @Autowired
     private UsernameListService usernameListService;
 
     @RequestMapping(value = "/username", method = RequestMethod.GET)
     public String getUsername(@RequestParam(value = "username", required = true) String username){
+        LOG.info(String.format("Searching username: %s", username));
         return usernameListService.searchUsername(username);
     }
 }
